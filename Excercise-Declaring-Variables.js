38bb3d2c1d3d971f783110a5bf18dbfc7c74cd8f
sammidev87 <sammidev87@gmail.com>
2023-09-24 19:51:46 -0500

    updating comments in Excercise-Declaring-Variables.js

--- Excercise-Declaring-Variables.js
+++ Excercise-Declaring-Variables.js
@@ -1,9 +1,21 @@
+/* the outputs should be as follows:
+'Rex'
+'Pepper'
+"My pet dog's name is Rex"
+"My pet cat's name is Pepper"
+*/
 var petDog = 'Rex';
 var petCat = 'Pepper';
 console.log(petDog);
 console.log(petCat);
 console.log("My pet dog's name is " + petDog);
 console.log("My pet cat's name is " + petCat);
+
+/* the outputs should be as follows:
+'Rex says woof'
+'Pepper says purr'
+'Pepper now says meow'
+*/
 var catSound = "purr";
 var dogSound = "woof";
 console.log(petDog, "says " + dogSound);
