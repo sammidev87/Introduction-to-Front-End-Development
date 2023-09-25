492f817212001e35bde815babd55bfa5dc772486
sammidev87 <sammidev87@gmail.com>
2023-09-24 20:45:26 -0500

    Exercise-Repetitive-Tasks-with-Loops.js Init

---
+++ Exercise-Repetitive-Tasks-with-Loops.js
@@ -0,0 +1,65 @@
+/* for loop that should have an output:
+1
+2
+3
+4
+5
+Counting Completed!
+*/
+for (var count = 1; count < 6; count++) {
+    console.log(count);
+}
+console.log("Counting Completed!");
+
+/* for loop that should have an output:
+5
+4
+3
+2
+1
+Countdown Finished!
+*/
+for (var count = 5; count >= 1; count--) {
+    console.log(count);
+}
+console.log("Countdown Finished!");
+
+/* while loop that should have an output:
+1
+2
+3
+4
+5
+Counting Completed!
+*/
+var count = 1;
+while (count < 6) {
+    console.log(count++);
+}
+console.log("Counting Completed!");
+
+/* while loop that should have an output:
+5
+4
+3
+2
+1
+Countdown Finished!
+*/
+var count = 5;
+while (count >= 1) {
+    console.log(count--);
+}
+console.log("Countdown Finished!");
+
+/* while loop that should have an output:
+2018
+2019
+2020
+2021
+2022
+*/
+var count = 2018;
+while (count < 2023) {
+    console.log(count++);
+}
