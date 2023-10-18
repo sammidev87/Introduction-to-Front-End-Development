387418592c88c784717722463885fd2f41a3f9a2
sammidev87 <sammidev87@gmail.com>
2023-10-18 16:51:18 -0500

    condensed code

--- Advanced-React/exercise-create-a-basic-list-component/src/DessertsList.js
+++ Advanced-React/exercise-create-a-basic-list-component/src/DessertsList.js
@@ -1,16 +1,11 @@
 function DessertsList(props) {
     // Implement the component here.
-    const under500Cal = props.data.filter((dessert) => dessert.calories <= 500);
-    const sortedCalList = under500Cal.sort((a, b) => a.calories - b.calories);
-    const dessertData = sortedCalList.map(dessert => {
-        const dessertText = `${dessert.name} - ${dessert.calories} cal`
-        return <li>{dessertText}</li>
-    })
-    return (
-        <ul>
-            {dessertData}
-        </ul>
-    );
-  }
-  
-  export default DessertsList;
+    const dessertData = props.data.filter((dessert) => { return dessert.calories <= 500 })
+        .sort((a, b) => { return a.calories - b.calories })
+        .map(dessert => {
+            return <li>${dessert.name} - ${dessert.calories} cal</li>
+        });
+    return <ul>{dessertData}</ul>;
+}
+
+export default DessertsList;
